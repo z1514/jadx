08b4b891eda88db8b3609379060ad9d8fdbcd1f5
z1514 <2498973668@qq.com>
2022-02-23 18:40:56 -0800

    change

--- jadx-gui/src/test/java/jadx/gui/JadxGUITest.java
+++ jadx-gui/src/test/java/jadx/gui/JadxGUITest.java
@@ -36,39 +36,39 @@ public class JadxGUITest {
 	/*
 	* Test class: JadxWrapper, JadxSettings, LogCollector, LafManager, NLS
 	* */
-	@Test
-	void testLaunch(){
-		try {
-			LogCollector.register();
-			JadxSettings settings = JadxSettingsAdapter.load();
-			settings.setLogLevel(LogHelper.LogLevelEnum.INFO);
-			// overwrite loaded settings by command line arguments
-			if (!settings.overrideProvided(new String[0])) {
-				return;
-			}
-			//init GUI theme
-//			LafManager.init(settings);
-			NLS.setLocale(settings.getLangLocale());
-
-			//init wrapper
-			JadxWrapper wrapper = new JadxWrapper(settings);
-			JadxProject jadxProject = new JadxProject();
-			jadxProject.setSettings(settings);
-			wrapper.setProject(jadxProject);
-
-			//test open dex file and init wrapper's decompiler
-			File file = new File("/Users/susunsheng/Downloads/hello1.dex");
-			List<Path> paths = FileUtils.toPaths(new File[]{file});
-			wrapper.openFile(paths);
-			RootNode root = wrapper.getDecompiler().getRoot();
-
-			ArgType argType = ArgType.tryToResolveClassAlias(root, ArgType.OBJECT);
-			ArgType argTypeCls = ArgType.tryToResolveClassAlias(root, ArgType.CLASS);
-			assertEquals(ArgType.OBJECT, argType);
-			assertEquals(ArgType.CLASS, argTypeCls);
-		} catch (Exception e) {
-			LOG.error("Error: {}", e.getMessage(), e);
-			System.exit(1);
-		}
-	}
+//	@Test
+//	void testLaunch(){
+//		try {
+//			LogCollector.register();
+//			JadxSettings settings = JadxSettingsAdapter.load();
+//			settings.setLogLevel(LogHelper.LogLevelEnum.INFO);
+//			// overwrite loaded settings by command line arguments
+//			if (!settings.overrideProvided(new String[0])) {
+//				return;
+//			}
+//			//init GUI theme
+////			LafManager.init(settings);
+//			NLS.setLocale(settings.getLangLocale());
+//
+//			//init wrapper
+//			JadxWrapper wrapper = new JadxWrapper(settings);
+//			JadxProject jadxProject = new JadxProject();
+//			jadxProject.setSettings(settings);
+//			wrapper.setProject(jadxProject);
+//
+//			//test open dex file and init wrapper's decompiler
+//			File file = new File("/Users/susunsheng/Downloads/hello1.dex");
+//			List<Path> paths = FileUtils.toPaths(new File[]{file});
+//			wrapper.openFile(paths);
+//			RootNode root = wrapper.getDecompiler().getRoot();
+//
+//			ArgType argType = ArgType.tryToResolveClassAlias(root, ArgType.OBJECT);
+//			ArgType argTypeCls = ArgType.tryToResolveClassAlias(root, ArgType.CLASS);
+//			assertEquals(ArgType.OBJECT, argType);
+//			assertEquals(ArgType.CLASS, argTypeCls);
+//		} catch (Exception e) {
+//			LOG.error("Error: {}", e.getMessage(), e);
+//			System.exit(1);
+//		}
+//	}
 }
