b7d5df9e2f5fd8d1f4494461cd5e7c2201b95065
susunsheng <su371128017@163.com>
2022-02-23 21:15:23 -0800

    style adjust

--- jadx-gui/src/test/java/jadx/gui/update/VersionComparatorTest.java
+++ jadx-gui/src/test/java/jadx/gui/update/VersionComparatorTest.java
@@ -9,62 +9,35 @@ import static org.hamcrest.Matchers.is;
 
 class VersionComparatorTest {
 
-
-
-	//if project tips not support ParameterizedTest,
+	// if project tips not support ParameterizedTest,
 	// need to refresh or reload project by Gradle tool
 	@ParameterizedTest
-	@CsvSource({
-			"1,2, -1"
-			,"v2.0,1.2, 1"
-			,"2.0,v2, 0"
-			,"v2.1,v2.1, 0"
-			,"02.1,2.1.0, 0"
-			,"v02.1,3, -1"
-			,"03,v2.1, 1"
-			,"v03,v2, 1"
-//			,"2.3,02, 1"
-			,"v2.2,02.2, 0"
-			,"2,v03, -1"
-			,"v1,v01, 0"
-			,"02,02.0, 0"
-			,"v02,02, 0"
-			,"3.03,v2.3, 1"
-			,"v02.3,v03, -1"
-	})
-	public void testCheckAndCompareNormalInput(String a, String b, int expected){
+	@CsvSource(
+		{
+				"1,2, -1", "v2.0,1.2, 1", "2.0,v2, 0", "v2.1,v2.1, 0", "02.1,2.1.0, 0", "v02.1,3, -1", "03,v2.1, 1", "v03,v2, 1"
+				// ,"2.3,02, 1"
+				, "v2.2,02.2, 0", "2,v03, -1", "v1,v01, 0", "02,02.0, 0", "v02,02, 0", "3.03, v2.3, 1", "v02.3,v03, -1"
+		}
+	)
+	public void testCheckAndCompareNormalInput(String a, String b, int expected) {
 		assertThat(VersionComparator.checkAndCompare(a, b), is(expected));
 	}
-
 
 	@ParameterizedTest
-	@CsvSource({
-//			"-3, 2, -2"
-//			,"-3, -2, -2"
-//			,"2, -3, -2"
-
-			"a, b, -2"
-			,"3, a, -2"
-			,"a, 2, -2"
-			,"A, B, -2"
-			,"3, A, -2"
-			,"A, 3, -2"
-			,"C, b, -2"
-			,"a, B, -2"
-
-			,"2147483647, 3, 1"
-			,"3, 2147483647, -1"
-			,"2147483648, 3, -2"
-			,"3, 2147483648, -2"
-			,"2147483648, 2147483649, -2"
-
-			,"%, *, -2"
-			,"%, #, -2"
-			,"*, %, -2"
-	})
-	public void testCheckAndCompareImproperInput(String a, String b, int expected){
+	@CsvSource(
+		{
+				// "-3, 2, -2"
+				// ,"-3, -2, -2"
+				// ,"2, -3, -2"
+				"a, b, -2", "3, a, -2", "a, 2, -2", "A, B, -2", "3, A, -2", "A, 3, -2", "C, b, -2", "a, B, -2",
+				"2147483647, 3, 1", "3, 2147483647, -1", "2147483648, 3, -2", "3, 2147483648, -2", "2147483648, 2147483649, -2",
+				"%, *, -2", "%, #, -2", "*, %, -2"
+		}
+	)
+	public void testCheckAndCompareImproperInput(String a, String b, int expected) {
 		assertThat(VersionComparator.checkAndCompare(a, b), is(expected));
 	}
+
 	@Test
 	public void testCompare() {
 		checkCompare("", "", 0);
