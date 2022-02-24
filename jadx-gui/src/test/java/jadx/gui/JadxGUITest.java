8bf36d30a912df15729a3bffa7af367e48a01b72
susunsheng <su371128017@163.com>
2022-02-23 21:51:55 -0800

    test gradlew

--- jadx-gui/src/test/java/jadx/gui/JadxGUITest.java
+++ jadx-gui/src/test/java/jadx/gui/JadxGUITest.java
@@ -1,28 +1,9 @@
 package jadx.gui;
 
-import jadx.cli.LogHelper;
-import jadx.core.dex.instructions.args.ArgType;
-import jadx.core.dex.nodes.RootNode;
-import jadx.gui.settings.JadxProject;
-import jadx.gui.settings.JadxSettings;
-import jadx.gui.settings.JadxSettingsAdapter;
-import jadx.gui.ui.MainWindow;
-import jadx.gui.utils.FileUtils;
-import jadx.gui.utils.LafManager;
-import jadx.gui.utils.NLS;
-import jadx.gui.utils.logs.LogCollector;
-import org.junit.jupiter.api.Test;
+import javax.swing.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.swing.*;
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @program: jadx
@@ -35,42 +16,42 @@ public class JadxGUITest {
 	private static final Logger LOG = LoggerFactory.getLogger(JadxGUITest.class);
 
 	/*
-	* Test class: JadxWrapper, JadxSettings, LogCollector, LafManager, NLS
-	* */
+	 * Test class: JadxWrapper, JadxSettings, LogCollector, LafManager, NLS
+	 */
 
-//	@Test
-//	void testLaunch(){
-//		try {
-//			LogCollector.register();
-//			JadxSettings settings = JadxSettingsAdapter.load();
-//			settings.setLogLevel(LogHelper.LogLevelEnum.INFO);
-//			// overwrite loaded settings by command line arguments
-//			if (!settings.overrideProvided(new String[0])) {
-//				return;
-//			}
-//			//init GUI theme
-////			LafManager.init(settings);
-//			NLS.setLocale(settings.getLangLocale());
-//
-//			//init wrapper
-//			JadxWrapper wrapper = new JadxWrapper(settings);
-//			JadxProject jadxProject = new JadxProject();
-//			jadxProject.setSettings(settings);
-//			wrapper.setProject(jadxProject);
-//
-//			//test open dex file and init wrapper's decompiler
-//			File file = new File(getResourcePath("hello1.dex"));
-//			List<Path> paths = FileUtils.toPaths(new File[]{file});
-//			wrapper.openFile(paths);
-//			RootNode root = wrapper.getDecompiler().getRoot();
-//
-//			ArgType argType = ArgType.tryToResolveClassAlias(root, ArgType.OBJECT);
-//			ArgType argTypeCls = ArgType.tryToResolveClassAlias(root, ArgType.CLASS);
-//			assertEquals(ArgType.OBJECT, argType);
-//			assertEquals(ArgType.CLASS, argTypeCls);
-//		} catch (Exception e) {
-//			LOG.error("Error: {}", e.getMessage(), e);
-//			System.exit(1);
-//		}
-//	}
+	// @Test
+	// void testLaunch(){
+	// try {
+	// LogCollector.register();
+	// JadxSettings settings = JadxSettingsAdapter.load();
+	// settings.setLogLevel(LogHelper.LogLevelEnum.INFO);
+	// // overwrite loaded settings by command line arguments
+	// if (!settings.overrideProvided(new String[0])) {
+	// return;
+	// }
+	// //init GUI theme
+	//// LafManager.init(settings);
+	// NLS.setLocale(settings.getLangLocale());
+	//
+	// //init wrapper
+	// JadxWrapper wrapper = new JadxWrapper(settings);
+	// JadxProject jadxProject = new JadxProject();
+	// jadxProject.setSettings(settings);
+	// wrapper.setProject(jadxProject);
+	//
+	// //test open dex file and init wrapper's decompiler
+	// File file = new File(getResourcePath("hello1.dex"));
+	// List<Path> paths = FileUtils.toPaths(new File[]{file});
+	// wrapper.openFile(paths);
+	// RootNode root = wrapper.getDecompiler().getRoot();
+	//
+	// ArgType argType = ArgType.tryToResolveClassAlias(root, ArgType.OBJECT);
+	// ArgType argTypeCls = ArgType.tryToResolveClassAlias(root, ArgType.CLASS);
+	// assertEquals(ArgType.OBJECT, argType);
+	// assertEquals(ArgType.CLASS, argTypeCls);
+	// } catch (Exception e) {
+	// LOG.error("Error: {}", e.getMessage(), e);
+	// System.exit(1);
+	// }
+	// }
 }
